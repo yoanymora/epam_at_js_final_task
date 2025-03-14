89b64036a133cc8765990d83cfb0c732f7d119be
Yoany Mora <yoany.stephany@gmail.com>
2025-03-14 17:57:56 -0600

    [IMP] Fulfill project requirements with basic tests
    Create tests for requirements UC-1 to UC-3 without using POM

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -3,4 +3,44 @@ describe("sauce demo page test suite", () => {
         await browser.url("https://www.saucedemo.com");
         await expect(browser).toHaveTitle("Swag Labs");
     });
-})
+
+    it("test login form with empty credentials", async () => {
+        await browser.url("https://www.saucedemo.com");
+        const userNameInput = await $('#user-name');
+        const passwordInput = await $('#password');
+        const logInButton = await $('#login-button');
+        const errorMessageContainer = await $('.error-message-container');
+        // clearValue() method is not working at all
+        // await userNameInput.addValue('test');
+        // await passwordInput.addValue('test');
+        // await userNameInput.clearValue();
+        // await passwordInput.clearValue();
+        await logInButton.click();
+        await expect(errorMessageContainer.$('h3')).toHaveText(expect.stringContaining("Username is required"));
+    });
+
+    it("test login form with credentials by passing user name", async () => {
+        await browser.url("https://www.saucedemo.com");
+        const userNameInput = await $('#user-name');
+        const passwordInput = await $('#password');
+        const logInButton = await $('#login-button');
+        const errorMessageContainer = await $('.error-message-container');
+        await userNameInput.addValue('test');
+        // await passwordInput.addValue('test');
+        // await passwordInput.clearValue()
+        await logInButton.click();
+        await expect(errorMessageContainer.$('h3')).toHaveText(expect.stringContaining("Password is required"));
+    });
+
+    it("test login form with credentials by passing user name and password", async () => {
+        await browser.url("https://www.saucedemo.com");
+        const userNameInput = await $('#user-name');
+        const passwordInput = await $('#password');
+        const logInButton = await $('#login-button');
+        await userNameInput.addValue('standard_user');
+        await passwordInput.addValue('secret_sauce');
+        await logInButton.click();
+        const headerLabel = await $('#header_container').$('.header_label').$('.app_logo');
+        await expect(headerLabel).toHaveText("Swag Labs");
+    });
+});
